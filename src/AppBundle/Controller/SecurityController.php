5bcdc53291f51055d438e0c8d79e269ee0080d34
linkouth <dkatimullin@gmail.com>
2018-07-25 17:18:04 +0200

    started making security conf

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linkouth
+ * Date: 25.07.18
+ * Time: 17:09
+ */
+
+namespace AppBundle\Controller;
+
+
+class SecurityController
+{
+
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linkouth
+ * Date: 25.07.18
+ * Time: 14:41
+ */
+
+namespace AppBundle\Entity;
+
+
+class User
+{
+
+}
